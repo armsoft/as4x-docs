eb16af2cc3e37f74ef297f262517ee355ddf0005
Stepan Hakobyan <stepan.hakobyan@live.com>
2018-08-25 18:34:30 +0400

    searching

--- assets/getindex.js
+++ assets/getindex.js
@@ -4,19 +4,25 @@ function getIndex() {
     xhr.onload = (event) => {
         if (event.target.status == 200) {
             let xml = xhr.responseXML.documentElement;
-            let arr = xml.getElementsByTagName("loc");
-            console.log(arr);
+            let arr = xml.getElementsByTagName("url");
+            
+            let search_input = document.getElementById("search_input");
 
             let content = document.getElementById("search_content");
+            content.innerHTML = "";
             for(let i = 0; i < arr.length; i++) {
-                //console.log(arr[i].lastChild.innerHTML);
-
-                let a = document.createElement("a");
-                a.href = "/learnmarkdown" + arr[i].innerHTML;
-                a.textContent = arr[i].innerHTML;
-                content.appendChild(a);
-                let br = document.createElement("br");
-                content.appendChild(br);
+                let urltag = arr[i];
+                let loc = urltag.getElementsByTagName("loc")[0].innerHTML;
+                let title = urltag.getElementsByTagName("title")[0].innerHTML;
+                let keywords = urltag.getElementsByTagName("keywords")[0].innerHTML;
+                if (keywords.includes(search_input.value)) {
+                    let a = document.createElement("a");
+                    a.href = "/learnmarkdown" + loc;
+                    a.textContent = title;
+                    content.appendChild(a);
+                    let br = document.createElement("br");
+                    content.appendChild(br);
+                }
             }
         } else {
             
