37f935e560b0042273e033eebadd6b5c232ad553
Stepan Hakobyan <stepan.hakobyan@live.com>
2018-08-25 15:34:36 +0400

    try js

---
+++ assets/getindex.js
@@ -0,0 +1,16 @@
+function getIndex() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "/learnmarkdown/links.xml", true);
+    xhr.onload = (event) => {
+        if (event.target.status == 200) {
+            let arr = xhr.responseXML.documentElement.getElementsByTagName("loc");
+            for(let el of arr) {
+                console.log(el.innerHTML);
+            }
+        } else {
+            
+        }
+    };
+    xhr.open()
+}
+
