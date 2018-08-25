db73a895182c281fea4960df92f29e0abe402949
Stepan Hakobyan <stepan.hakobyan@live.com>
2018-08-25 18:08:03 +0400

    try search

--- assets/getindex.js
+++ assets/getindex.js
@@ -3,10 +3,18 @@ function getIndex() {
     xhr.open("GET", "/learnmarkdown/links.xml", true);
     xhr.onload = (event) => {
         if (event.target.status == 200) {
-            let arr = xhr.responseXML.documentElement.getElementsByTagName("loc");
+            let xml = xhr.responseXML.documentElement;
+            let arr = xml.getElementsByTagName("loc");
             console.log(arr);
+
+            let content = document.getElementById("search_content");
             for(let i = 0; i < arr.length; i++) {
-                console.log(arr[i].innerHTML);
+                //console.log(arr[i].lastChild.innerHTML);
+
+                let a = document.createElement("a");
+                a.href = arr[i].innerHTML;
+                a.textContent = arr[i].innerHTML;
+                content.appendChild(a);
             }
         } else {
             
