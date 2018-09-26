9950c3bf12d55f1ca913ae13a10a23bb5f6a9af5
Stepan Hakobyan <stepan.hakobyan@live.com>
2018-09-26 11:49:45 +0400

    index1

--- assets/getindex.js
+++ assets/getindex.js
@@ -1,34 +1,51 @@
 function getIndex() {
     let xhr = new XMLHttpRequest();
-    xhr.open("GET", "/learnmarkdown/links.xml", true);
-    xhr.onload = (event) => {
-        if (event.target.status == 200) {
-            let xml = xhr.responseXML.documentElement;
-            let arr = xml.getElementsByTagName("url");
+    // xhr.open("GET", "/learnmarkdown/links.xml", true);
+    // xhr.onload = (event) => {
+    //     if (event.target.status == 200) {
+    //         let xml = xhr.responseXML.documentElement;
+    //         let arr = xml.getElementsByTagName("url");
             
-            let search_input = document.getElementById("search_input");
+    //         let search_input = document.getElementById("search_input");
 
-            let content = document.getElementById("search_content");
-            content.innerHTML = "";
-            for(let i = 0; i < arr.length; i++) {
-                let urltag = arr[i];
-                let loc = urltag.getElementsByTagName("loc")[0].innerHTML;
-                let title = urltag.getElementsByTagName("title")[0].innerHTML;
-                let keywords = urltag.getElementsByTagName("keywords")[0].innerHTML;
-                if (keywords.toUpperCase().includes(search_input.value.toUpperCase())) {
-                    let a = document.createElement("a");
-                    a.href = "/learnmarkdown" + loc;
-                    a.textContent = title;
-                    content.appendChild(a);
-                    let br = document.createElement("br");
-                    content.appendChild(br);
-                }
-            }
-        } else {
+    //         let content = document.getElementById("search_content");
+    //         content.innerHTML = "";
+    //         for(let i = 0; i < arr.length; i++) {
+    //             let urltag = arr[i];
+    //             let loc = urltag.getElementsByTagName("loc")[0].innerHTML;
+    //             let title = urltag.getElementsByTagName("title")[0].innerHTML;
+    //             let keywords = urltag.getElementsByTagName("keywords")[0].innerHTML;
+    //             if (keywords.toUpperCase().includes(search_input.value.toUpperCase())) {
+    //                 let a = document.createElement("a");
+    //                 a.href = "/learnmarkdown" + loc;
+    //                 a.textContent = title;
+    //                 content.appendChild(a);
+    //                 let br = document.createElement("br");
+    //                 content.appendChild(br);
+    //             }
+    //         }
+    //     } else {
             
-        }
-    };
-    xhr.send();
+    //     }
+    // };
+    // xhr.send();
+
+    
+    let content = document.getElementById("index_content");
+    content.innerHTML = "";
+    var pages = tipuesearch.pages;
+
+    for(let i = 0; i < pages.length; i++) {
+        let urltag = pages[i];
+        let loc = urltag.url;
+        let title = urltag.title ? urltag.title : urltag.url;
+        let a = document.createElement("a");
+        a.href = loc;
+        a.textContent = title;
+        content.appendChild(a);
+        let br = document.createElement("br");
+        content.appendChild(br);
+    }
 }
 
 // let search_form = document.getElementById("search_form");
