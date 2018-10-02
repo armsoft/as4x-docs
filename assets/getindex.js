d24710b8e0b39cbe2cf539d42df430be6d479557
Stepan Hakobyan <stepan.hakobyan@live.com>
2018-10-02 11:37:40 +0400

    index

--- assets/getindex.js
+++ assets/getindex.js
@@ -19,17 +19,24 @@ function getIndex() {
 window.addEventListener("load", (ev) => {
     getIndex();
     let input = document.getElementById("tipue_search_input");
-    input.addEventListener("keydown", (ev) => {
-        let content = document.getElementById("index_content");
-        let searchText = input.value;
-        let aArr = content.getElementsByTagName("a");
-        for (let i = 0; i < aArr.length; i++) {
-            aTag = aArr[i];
-            if (aArr[i].textContent.indexOf(searchText)) {
-                aArr[i].parentElement.style.display = "";
-            } else {
-                aArr[i].parentElement.style.display = "none";
-            }
-        }
+    input.addEventListener("keyup", (ev) => {
+        searchText(input);
+    });
+    input.addEventListener("change", (ev) => {
+        searchText(input);
     });
 });
+
+function searchText(input) {
+    let content = document.getElementById("index_content");
+    let searchText = input.value.toLowerCase();
+    console.log(searchText);
+    let aArr = content.getElementsByTagName("a");
+    for (let i = 0; i < aArr.length; i++) {
+        if (aArr[i].textContent.toLowerCase().indexOf(searchText) >= 0) {
+            aArr[i].parentElement.style.display = "";
+        } else {
+            aArr[i].parentElement.style.display = "none";
+        }
+    }
+}
