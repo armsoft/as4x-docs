266f34804ab92cfa8563ce2548ecb3871fcc5600
Stepan Hakobyan <stepan.hakobyan@live.com>
2018-12-05 16:51:58 +0400

    Dark Updated table. Cookie -> localStorage

--- assets/manageThemes.js
+++ assets/manageThemes.js
@@ -1,30 +1,32 @@
-let csstag = document.getElementById("darktheme");
-if (document.cookie.indexOf("theme:dark") >= 0) {
-    csstag.rel = "stylesheet";
-} else {
-    csstag.rel = "";
-}
-
-window.addEventListener("load", () => {
-    let goDark = document.createElement("a");
-    goDark.href = "#";
-    goDark.classList.add("page-link");
-    goDark.style.filter = "grayscale(100%)";
-    if (document.cookie.indexOf("theme:dark") >= 0) {
-        goDark.addEventListener("click", () => {
-            document.cookie = "theme:light";
-            document.location.reload();
-        });
-        goDark.innerHTML = "\u{2600}";  //BLACK SUN WITH RAYS
-        goDark.title = "Light";
+if (window.localStorage) {
+    let csstag = document.getElementById("darktheme");
+    if (window.localStorage["theme"] === "dark") {
+        csstag.rel = "stylesheet";
     } else {
-        goDark.addEventListener("click", () => {
-            document.cookie = "theme:dark";
-            document.location.reload();
-        });
-        goDark.innerHTML = "\u{1F319}"; //CRESCENT MOON
-        goDark.title = "Dark";
+        csstag.rel = "";
     }
-    let nav = document.querySelector(".site-nav .trigger");
-    nav.appendChild(goDark);
-});
+
+    window.addEventListener("load", () => {
+        let goDark = document.createElement("a");
+        goDark.href = "#";
+        goDark.classList.add("page-link");
+        goDark.style.filter = "grayscale(100%)";
+        if (window.localStorage["theme"] === "dark") {
+            goDark.addEventListener("click", () => {
+                window.localStorage["theme"] = "light";
+                document.location.reload();
+            });
+            goDark.innerHTML = "\u{2600}";  //BLACK SUN WITH RAYS
+            goDark.title = "Light";
+        } else {
+            goDark.addEventListener("click", () => {
+                window.localStorage["theme"] = "dark";
+                document.location.reload();
+            });
+            goDark.innerHTML = "\u{1F319}"; //CRESCENT MOON
+            goDark.title = "Dark";
+        }
+        let nav = document.querySelector(".site-nav .trigger");
+        nav.appendChild(goDark);
+    });
+}
